044b6f28c83174849a208da0ba0e78fba8655d39
Ramprasath Babu <ramprasath.technology@gmail.com>
2024-12-21 22:17:07 -0600

    Add health check endpoint

---
+++ Currency/Controllers/HealthCheck/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Currency.Controllers.HealthCheck
+{
+    [ApiVersion("1.0")]
+    [Route("v{v:apiVersion}/HealthCheck")]
+    [ApiController]
+    public class HealthCheckV1Controller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetHealthCheck()
+        {
+            return Ok("The app is running");
+        }
+    }
+}
